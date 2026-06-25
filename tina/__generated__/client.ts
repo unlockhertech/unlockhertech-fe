03e478f4871b04ffb19cea3a9eba2e7691fe523f
etaXeta <eetalocs@gmail.com>
2026-06-25 20:44:04 +0100

    fix: stop tracking tina generated assets

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types.js";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '2c585d925306d419ad60010e2a2a898d312d8105', queries,  });
-export default client;
-  
